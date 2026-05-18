d4b22f6b0e245157f14bb12d5a9a231fb9ac98d7
bernardo <bernardo.queiroz01@gmail.com>
2026-05-18 17:00:08 -0300

    Primeiro commit

--- src/App.java
+++ src/App.java
@@ -1,5 +1,15 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Pet pet = new Pet();
+        pet.nome = "rex";
+        pet.especie = "cachorro";
+        pet.raca = "Labrador";
+        pet.idade = 3;
+        pet.peso = 28.5;
+
+        pet.apresentar();
+
+        pet.avaliarPeso(30.0);
+
     }
 }
